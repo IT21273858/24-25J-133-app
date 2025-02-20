5758cfc00edc1cac9154ee8d4ecf7c54d987dd59
IT21273858 <it21273858@my.sliit.lk>
2025-02-21 01:07:10 +0530

    project initialised

---
+++ dyslexia/lib/variables.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// App information
+const String appTitle = 'Dyslexia';
+const int appVersion = 1;
+const String appAuthor = 'Flutter Team';
+const String appDescription =
+    'Mobile and Simulation-based Approach to reduce Dyslexia with children Learning Disabilities';
+const String appLicense = 'MIT License';
+const String appCopyright =
+    'Copyright (c) RP 24-25-133 Team. All rights reserved.';
+const String appUrl = 'https://flutter.dev';
+
+// Asset paths
+const String appIcon = 'assets/images/app_icon.png';
+const String appBackground = 'assets/images/app_background.jpg';
+
+// Colors
+const Color appPrimaryColor = Color(0xFF9370DB); // Purple
+const Color appSecondaryColor = Color(0xFF554997); // Darker purple
+const Color appTextColor = Color(0xFF4F4B65);
+const Color appBackgroundColor = Color(0xFFF0EFF4);
